af7660e380936a7bfab1b81207586377814bdbee
Aavan Vostro <alirezainallo@gmail.com>
2026-07-13 02:39:45 +0330

    try to find win key

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 USBHIDKeyboard Keyboard;
 
 uint32_t lastAction = 0;
-const uint32_t interval = 5000; // 5 seconds
+const uint32_t interval = 1000; // 5 seconds
 
 
 void setup()
@@ -19,7 +19,39 @@ void setup()
     Serial.println("USB Keyboard Started");
 }
 
+void keyChecker()
+{
+    static uint32_t cnter = 0;
+  Serial.println("Opening Chrome...");
+  
+  // Press Windows key
+  // Keyboard.press(KEY_LEFT_GUI); // Windows key (left)
+  // delay(50);
+  // Keyboard.release(KEY_LEFT_GUI); // Release Windows key
+  // Keyboard.press(KEY_RIGHT_GUI); // Windows key (left)
+  // delay(50);
+  // Keyboard.release(KEY_RIGHT_GUI); // Release Windows key
+  Keyboard.printf("cnter: %d:", cnter);
+  
+  delay(200);
 
+  Keyboard.write(cnter);  // write یعنی press + release با هم
+//   Keyboard.write(KEY_LEFT_GUI);  // write یعنی press + release با هم
+
+  delay(200);
+  
+  // Type chrome
+  
+  // Press Enter
+  // Keyboard.press(KEY_RETURN);
+  // delay(50);
+  // Keyboard.release(KEY_RETURN);
+  Keyboard.write(KEY_RETURN);  // write یعنی press + release با همchrome
+  
+  cnter += 1;
+
+  Serial.println("Done");
+}
 
 
 void openChrome()
@@ -33,7 +65,8 @@ void openChrome()
   // Keyboard.press(KEY_RIGHT_GUI); // Windows key (left)
   // delay(50);
   // Keyboard.release(KEY_RIGHT_GUI); // Release Windows key
-  Keyboard.write(KEY_LEFT_GUI);  // write یعنی press + release با هم
+  Keyboard.write(93);  // write یعنی press + release با هم
+//   Keyboard.write(KEY_LEFT_GUI);  // write یعنی press + release با هم
 
   delay(500);
   
@@ -59,6 +92,7 @@ void loop()
     {
         lastAction = now;
 
-        openChrome();
+        // openChrome();
+        keyChecker();
     }
 }
